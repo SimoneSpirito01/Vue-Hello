daf2630b435fee28674c3d7d86f09429153a75fe
SimoneSpirito <simospiri01@gmail.com>
2021-11-25 16:01:55 +0100

    h1 + img with Vue.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ const app = new Vue(
+     {
+         el: '#root',
+         data: {
+             title: 'Hello World',
+             mySrc: 'https://picsum.photos/800/800'
+         }
+     }
+     
+ )
